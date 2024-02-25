ba721735523008256a543a92cbd1f991ac6ba9a4
Jared Revier <119819370+dahedgetrimmer@users.noreply.github.com>
2024-02-25 16:47:29 -0600

    mf6_tri_mesh_nonest.py

--- mf6_tri_mesh_nonest.py
+++ mf6_tri_mesh_nonest.py
@@ -23,10 +23,9 @@ starttime = time.time()
 cpustart= time.process_time()
 
 name = "tri_proj_nonest"
-workspace = '/home/jrevier/Documents/ahs432/Final_Project/Model/nonest'
-tri_exe = '/home/jrevier/Documents/ahs432/Final_Project/Exe/triangle'
-mf6_exe = '/home/jrevier/Documents/ahs432/Final_Project/Exe/mf6'
-
+workspace = 'PATH_TO_SIMULATION_OUTPUT'
+tri_exe = 'PATH_TO_triangle_EXECUTABLE'
+mf6_exe = 'PATH_TO_mf6_EXECUTABLE'
 
 
 #Creating the Aquifer Domain
@@ -212,7 +211,7 @@ colorbar.set_label('Head Elevations (m)', fontsize='15')
 endtime = time.time()
 cpuend= time.process_time()
 
-plt.savefig('/home/jrevier/Documents/ahs432/Final_Project/Plots/nonest_example', 
+plt.savefig('PATH_TO_....', 
             format='png')
 
 
